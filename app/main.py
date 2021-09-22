1efb42e8b24944e7d071f473cb7f65a4307ff38b
Tan Zuñiga 🚀 <taniazuniga.mx@gmail.com>
2021-09-21 19:04:56 -0500

    🎨 Improve app UI and query translation
    + Open images as modals
    + Natural Language to interpret the queries.
    + Add footer
    + Improve instructions and more info about the project
    + Add mood recognition

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import logging
 import os
+from datetime import datetime
 
-from flask import Flask, render_template, request
 import google.cloud.logging
+from flask import Flask, render_template, request
 from google.cloud import firestore
+from google.cloud import language_v1 as language
 from google.cloud import storage
 from google.cloud import translate_v2 as translate
 
@@ -12,7 +14,7 @@ client.get_default_handler()
 client.setup_logging()
 
 app = Flask(__name__)
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def root():
@@ -49,49 +51,94 @@ def search():
     results = []
 
     if query:
-        db = firestore.Client()
-        original_doc = db.collection(u'tags').document(query.lower()).get().to_dict()
-        if original_doc is None:
-            original_doc = {}
+        queries = translate_query(query)
+        # if the input is a phrase
+        if len(query.split(" ")) > 1:
+            queries += get_important_words_from_phrase(queries)
 
-        doc_en = translate_query(query, db)
-        if doc_en is None:
-            doc_en = {}
+        doc = get_collections(queries)
 
         try:
-            doc = {**original_doc, **doc_en}
             for url in doc['photo_urls']:
                 results.append(url)
-                results = list(dict.fromkeys(results))
-        except TypeError as e:
+            results = list(dict.fromkeys(results))
+        except TypeError as _e:
             pass
-        except KeyError as e:
+        except KeyError as _e:
             pass
     return render_template('search.html', query=query, results=results)
 
 
-def translate_query(query, db):
-    doc_en = {}
+def get_collections(queries):
+    db = firestore.Client()
+    all_docs = {}
+    for query in queries:
+        doc = db.collection(u'tags').document(query.lower()).get().to_dict()
+        if doc is not None:
+            all_docs = {**all_docs, **doc}
+
+    return all_docs
+
+
+def get_important_words_from_phrase(queries):
+    all_kw = []
+    lang_client = language.LanguageServiceClient()
+    for query in queries:
+        type_ = language.Document.Type.PLAIN_TEXT
+        document = {"content": query, "type_": type_}
+        encoding_type = language.EncodingType.UTF8
+        all_kw += get_entities(lang_client, document, encoding_type)
+        all_kw += get_keywords(lang_client, document, encoding_type)
+
+    return all_kw
+
+
+def get_entities(lang_client, document, encoding_type):
+    entities = []
+    response = lang_client.analyze_entities(document=document, encoding_type=encoding_type)
+    for entity in response.entities:
+        entities.append(entity.name)
+        if language.Entity.Type(entity.type_).name != "OTHER":
+            entities.append(language.Entity.Type(entity.type_).name)
+    return entities
+
+
+def get_keywords(lang_client, document, encoding_type):
+    entities = []
+    response = lang_client.analyze_syntax(document=document, encoding_type=encoding_type)
+    for token in response.tokens:
+        part_of_speech = language.PartOfSpeech.Tag(token.part_of_speech.tag).name
+        if part_of_speech == "NOUN":
+            entities.append(token.text.content)
+    return entities
+
+
+def translate_query(query):
+    queries = [query]
     translate_client = translate.Client()
-    result = translate_client.detect_language(query)["language"]
+    lang = translate_client.detect_language(query)["language"]
 
-    if result != "en":
-        en_query = translate_client.translate(query, target_language="en")['translatedText']
-        doc_en = db.collection(u'tags').document(en_query.lower()).get().to_dict()
+    if lang != "en":
+        queries.append(translate_client.translate(query, target_language="en")['translatedText'])
 
-    return doc_en
+    return queries
 
 
 @app.errorhandler(500)
-def server_error(e):
+def server_error(_e):
     logging.exception('An error occurred during a request.')
     return render_template('error.html', error=500), 500
 
 
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(_e):
     logging.exception('An error occurred during a request.')
     return render_template('error.html', error=404), 404
+
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
 
 
 if __name__ == '__main__':
--- function/main.py
+++ function/main.py
@@ -17,29 +17,27 @@ def _analyze_photo(bucket, file_name):
     image = vision.Image(source=vision.ImageSource(image_uri=f'gs://{bucket}/{file_name}'))
     tags = _get_all_labels(client, image)
 
+    print(f"Tags: {tags} \nfor Image: gs://{bucket}/{file_name}")
+
     return tags
 
 
 def _get_all_labels(client, image):
     # get Objects
     objects = client.object_localization(image=image).localized_object_annotations
-    print(objects)
     # get Label
     labels = client.label_detection(image=image).label_annotations
-    print(labels)
     # get Logos
     logos = client.logo_detection(image=image).logo_annotations
-    print(logos)
     # get Landmarks
     places = client.landmark_detection(image=image).landmark_annotations
-    print(places)
     # get bestGuessLabels
     annotations = client.web_detection(image=image).web_detection.best_guess_labels
-    print(annotations)
     # get text
     texts = client.text_detection(image=image).text_annotations
-    print(texts)
     hand_write = client.document_text_detection(image=image).text_annotations
+    # get mood
+    likelihoods = get_likelihood(client, image)
 
     # merge all labels
     tags = []
@@ -50,8 +48,45 @@ def _get_all_labels(client, image):
     tags += map(lambda annotation: annotation.label.lower(), annotations)
     tags += map(lambda text: text.description.lower(), texts)
     tags += map(lambda text: text.description.lower(), hand_write)
+    tags += map(lambda likelihood: likelihood.lower(), likelihoods)
 
+    # remove duplicate
+    tags = list(dict.fromkeys(tags))
     return tags
+
+
+def get_likelihood(client, image):
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+
+    likelihoods = []
+    person_count = 0
+
+    for face in faces:
+        person_count += 1
+        likelihoods.append('person')
+
+        if likelihood_name[face.anger_likelihood]:
+            likelihoods.extend(['anger', 'angry'])
+
+        if likelihood_name[face.joy_likelihood]:
+            likelihoods.extend(['happy', 'smile', 'joy'])
+
+        if likelihood_name[face.sorrow_likelihood]:
+            likelihoods.extend(['sorrow', 'sad', 'cry'])
+
+        if likelihood_name[face.surprise_likelihood]:
+            likelihoods.extend(['surprise', 'surprised person'])
+
+    if person_count == 1:
+        likelihoods.append(f"person")
+    elif person_count > 1:
+        likelihoods.extend([f"{person_count} people", "people"])
+
+    return likelihoods
 
 
 def _store_results(bucket, file_name, tags):
